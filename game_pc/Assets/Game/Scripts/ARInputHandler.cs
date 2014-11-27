0506d044f691418db84fed58083141a8cf8d280c
if1live <libsora25@gmail.com>
2014-11-27 21:56:38 +0900

    1,2,3 누르면 씬 교체. 근데 내용이 없다

--- game_pc/Assets/Game/Scripts/ARInputHandler.cs
+++ game_pc/Assets/Game/Scripts/ARInputHandler.cs
@@ -5,6 +5,14 @@ using InControl;
 public class ARInputHandler : MonoBehaviour {
     private float sceneSwitchDelay = 0.5f;
 
+    public GameObject scene1;
+    public GameObject scene2;
+    public GameObject scene3;
+
+    void Start()
+    {
+        RunScene1();
+    }
 	// Update is called once per frame
 	void Update () {
         sceneSwitchDelay -= Time.deltaTime;
@@ -18,5 +26,41 @@ public class ARInputHandler : MonoBehaviour {
         {
             BaseCommand.GameSceneSwitch().Run();
         }
+
+        // handle keyboard for gui
+        if (Input.GetButtonDown("Scene1"))
+        {
+            this.RunScene1();   
+        }
+        else if(Input.GetButtonDown("Scene2"))
+        {
+            this.RunScene2();
+        }
+        else if (Input.GetButtonDown("Scene3"))
+        {
+            this.RunScene3();
+        }
 	}
+
+    void RunScene1()
+    {
+        Debug.Log("Scene1");
+        scene1.SetActive(true);
+        scene2.SetActive(false);
+        scene3.SetActive(false);
+    }
+    void RunScene2()
+    {
+        Debug.Log("Scene2");
+        scene1.SetActive(false);
+        scene2.SetActive(true);
+        scene3.SetActive(false);
+    }
+    void RunScene3()
+    {
+        Debug.Log("Scene3");
+        scene1.SetActive(false);
+        scene2.SetActive(false);
+        scene3.SetActive(true);
+    }
 }
